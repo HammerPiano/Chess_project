671fb7a555f7ac2b45b49e28b84e3fc41f72b353
ShaiM11 <34599163+ShaiM11@users.noreply.github.com>
2017-12-27 19:30:43 +0200

    Update Queen

--- Chess_project/Queen.cpp
+++ Chess_project/Queen.cpp
@@ -8,7 +8,112 @@ Queen::~Queen()
 {
 }
 
-void Queen::move(Point coordinates)
+void Queen::move(Point coordinates, const vector<vector<Piece*>>& board)
 {
-
+	char curr_Y = this->_coordinates.getX();
+	char curr_X = this->_coordinates.getY();
+	char new_Y = coordinates.getX();
+	char new_X = coordinates.getY();
+	bool retVal = true;
+	int i = 0, j = 0;
+	if (curr_Y<new_Y && curr_X == new_X)
+	{
+		for (i = curr_Y + 1; i<new_Y && retVal == true; i++)
+		{
+			if (board[i][new_X] != nullptr)
+			{
+				retVal = false;
+			}
+		}
+	}
+	else if (curr_Y>new_Y && curr_X == new_X)
+	{
+		for (i = curr_Y - 1; i>new_Y && retVal == true; i--)
+		{
+			if (board[i][new_X] != nullptr)
+			{
+				retVal = false;
+			}
+		}
+	}
+	else if (curr_X>new_X && curr_Y == new_Y)
+	{
+		for (j = curr_X - 1; j>new_X && retVal == true; j--)
+		{
+			if (board[curr_Y][j] != nullptr)
+			{
+				retVal = false;
+			}
+		}
+	}
+	else if (curr_X<new_X && curr_Y == new_Y)
+	{
+		for (j = curr_X + 1; j<new_X && retVal == true; j++)
+		{
+			if (board[curr_Y][j] != nullptr)
+			{
+				retVal = false;
+			}
+		}
+	}
+	else if (curr_Y<new_Y && curr_X<new_X) // right and down
+	{
+		i = curr_Y + 1;
+		j = curr_X + 1;
+		while (i<new_Y && j<new_X && retVal == true)
+		{
+			if (board[i][j] != nullptr)
+			{
+				retVal = false;
+			}
+			i++;
+			j++;
+		}
+	}
+	else if (curr_Y>new_Y && curr_X<new_X) // right and up
+	{
+		i = curr_Y - 1;
+		j = curr_X + 1;
+		while (i>new_Y && j<new_X && retVal == true)
+		{
+			if (board[i][j] != nullptr)
+			{
+				retVal = false;
+			}
+			i--;
+			j++;
+		}
+	}
+	else if (curr_Y>new_Y && curr_X>new_X) // left and up
+	{
+		i = curr_Y - 1;
+		j = curr_X - 1;
+		while (i>new_Y && j>new_X && retVal == true)
+		{
+			if (board[i][j] != nullptr)
+			{
+				retVal = false;
+			}
+			i--;
+			j--;
+		}
+	}
+	else if (curr_Y<new_Y && curr_X>new_X) // left and down
+	{
+		i = curr_Y + 1;
+		j = curr_X - 1;
+		while (i<new_Y && j>new_X && retVal == true)
+		{
+			if (board[i][j] != nullptr)
+			{
+				retVal = false;
+			}
+			i++;
+			j--;
+		}
+	}
+	if (retVal)
+	{
+		this->_coordinates.setXY = coordinates;
+	}
 }
